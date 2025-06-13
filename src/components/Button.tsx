752a8661ed67bc49fa322b9ddb6911e1330a203f
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-13 09:00:55 +0700

    feat(ui): add Button component to design system

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+const getVariantStyles = (
+  variant: NonNullable<ButtonProps['variant']>,
+  disabled?: boolean
+) => {
+  const styles: Record<NonNullable<ButtonProps['variant']>, string> = {
+    primary: disabled
+      ? 'bg-[#DDDDDC] text-[#3E3E3C]'
+      : 'bg-[#397832] text-[#FAFCFE] hover:bg-[#10490A]',
+    secondary: disabled
+      ? 'border-2 bg-[#DDDDDC] border-[#3E3E3C] text-[#3E3E3C]'
+      : 'border-2 bg-[#FAFCFE] text-[#397832] border-[#397832] hover:bg-[#F8F8EF]',
+    tonal: 'bg-[#FBF6C7] text-[#9A9353] hover:bg-[#F6E96B]',
+    confirm: 'bg-[#4FC65F] text-[#FAFCFE] hover:bg-[#33803F]',
+    delete: 'bg-[#E64341] text-[#FAFCFE] hover:bg-[#B11E1B]',
+    cancle: 'bg-[#7C7C77] text-[#FAFCFE] hover:bg-[#3E3E3C]',
+  };
+
+  return styles[variant];
+};
+
+
+
+
+type ButtonProps = {
+  variant?: 'primary' | 'secondary' |'tonal' | 'confirm' | 'delete' | 'cancle';
+  text:string,
+  disabled?: boolean,
+  onClick?:()=>void,
+};
+
+
+
+export default function Button({variant='primary',text,disabled = false,onClick}:ButtonProps){
+    return(
+        <button onClick={onClick} disabled={disabled} className={` w-[72px] h-[48px] //p-3 rounded-2xl font-prompt text-base flex items-center justify-center ${getVariantStyles(variant, disabled)} 
+        // ${disabled ? "" : "active:scale-95 cursor-pointer"} transition-all duration-100 ease-in-out` }>
+            {text}
+        </button>
+    );
+}
