1ed716cb615aef0f87e0f8dc82dd1085cfde4bf2
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-18 14:36:53 +0700

    feat(ui): layout manage-data page based on wireframe

--- src/app/manage-data/page.tsx
+++ src/app/manage-data/page.tsx
@@ -1,13 +1,32 @@
+import Button from "@/components/Button";
+import InputField from "@/components/InputField";
 import TestCard from "@/components/Testcard";
 import Link from "next/link";
+import Icons from "@/components/svgs/SvgExports";
+import DataTableCard from "./components/DataTableCard";
+import { DataGroup } from "../../../types";
+import { mockDataGroups } from "./components/mockDataGroups";
 
 
 
 export default function ManageData() {
+
   return (
-    <main className="flex flex-col items-center justify-center p-4 sm:p-6 md:p-8">
-      <TestCard link="/manage-data/forest" text="Forest"></TestCard>
-      <TestCard link="/manage-data/land" text="Land"></TestCard>
-    </main>
+    <div className=" flex flex-col  justify-center">
+    <div className="w-full  flex flex-col  py-[32px] px-[148px]">
+        <div className=" w-full h-[40px]  font-prompt text-[#27272A] text-2xl font-medium flex  justify-end  whitespace-nowrap ">
+              ระบบฐานข้อมูลการสำรวจศักยภาพการกักเก็บคาร์บอนในพื้นที่ป่าไม้
+        </div>
+        <div className=" w-full h-[40px]  font-prompt text-[#27272A] items-center font-medium flex  flex-row justify-between my-[24px] whitespace-nowrap ">
+              <div className="text-xl">จัดการข้อมูล</div>
+              <div ><InputField iconSearch  className="h-[32px] w-[287px] rounded-2xl" /></div>
+              <div><Button text="เพิ่มข้อมูล" className="w-[113px] h-[40px] rounded-lg "><Icons.Add /></Button></div>
+        </div>
+        <DataTableCard data={mockDataGroups} group/>
+    </div>
+    </div>
+    
+  
+  
   );
 }
