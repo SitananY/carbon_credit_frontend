9e1890691a3b8a3440279c5454fc278735e65be7
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-18 15:05:17 +0700

    chore(ui): fix text Wrap in manage-data page

--- src/app/manage-data/page.tsx
+++ src/app/manage-data/page.tsx
@@ -13,14 +13,14 @@ export default function ManageData() {
 
   return (
     <div className=" flex flex-col  justify-center">
-    <div className="w-full  flex flex-col  py-[32px] px-[148px]">
+    <div className="w-full  flex flex-col  py-[32px] xl:px-[148px] px-[32px]">
         <div className=" w-full h-[40px]  font-prompt text-[#27272A] text-2xl font-medium flex  justify-end  whitespace-nowrap ">
               ระบบฐานข้อมูลการสำรวจศักยภาพการกักเก็บคาร์บอนในพื้นที่ป่าไม้
         </div>
-        <div className=" w-full h-[40px]  font-prompt text-[#27272A] items-center font-medium flex  flex-row justify-between my-[24px] whitespace-nowrap ">
-              <div className="text-xl">จัดการข้อมูล</div>
-              <div ><InputField iconSearch  className="h-[32px] w-[287px] rounded-2xl" /></div>
-              <div><Button text="เพิ่มข้อมูล" className="w-[113px] h-[40px] rounded-lg "><Icons.Add /></Button></div>
+        <div className=" w-full h-[40px]  font-prompt text-[#27272A] items-center  flex  flex-row justify-between my-[24px] whitespace-nowrap ">
+              <div className="w-auto h-[40px] text-xl font-medium">จัดการข้อมูล</div>
+              <div className="w-auto h-[40px]"><InputField iconSearch  className="h-[32px] w-[287px] rounded-2xl" /></div>
+              <div className="w-auto h-[40px]"><Button text="เพิ่มข้อมูล" className="w-[113px] h-[40px] rounded-lg "><Icons.Add /></Button></div>
         </div>
         <DataTableCard data={mockDataGroups} group/>
     </div>
