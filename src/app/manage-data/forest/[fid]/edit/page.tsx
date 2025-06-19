f4f75a219caa3b76d590464e2cdd1e2c1bc41052
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-19 17:30:48 +0700

    feat(ui): layout forest and land [fid] edit page based on wireframe

--- src/app/manage-data/forest/[fid]/edit/page.tsx
+++ src/app/manage-data/forest/[fid]/edit/page.tsx
@@ -1,15 +1,46 @@
+'use client'
+import { mockDataGroups } from "../../.././components/mockDataGroups";
+import { useParams, useRouter } from "next/navigation";
+import { ForestData, LandData } from "../../../../../../types";
+import SectionBelowHeader from "@/app/manage-data/components/SectionBelowHeader";
+import SectionHeader from "@/app/manage-data/components/SectionHeader";
 
 
 
 export default function Edit() {
+  const { fid } = useParams();
+  const idFromParam = Number(fid);
+
+  let matchedItem: ForestData | LandData | null = null;
+
+  for (const group of mockDataGroups) {
+    if (group.forestData) {
+      matchedItem = group.forestData.find(item => item.id === idFromParam)??null;
+      if (matchedItem) break;
+    }
+    if (group.LandData) {
+      matchedItem = group.LandData.find(item => item.id === idFromParam)??null;
+      if (matchedItem) break;
+    }
+  }
+  if(!matchedItem) return null;
+  const isForest = "tree_type" in matchedItem;
+  const router = useRouter();
+
   return (
-    <main className="flex  items-center justify-center p-4 sm:p-6 md:p-8">
-      <div className="w-full max-w-md h-64 bg-white rounded-xl shadow-2xl flex items-center justify-center p-4">
-          <div className=" font-mono text-3xl font-bold text-center">
-          Edit
-          </div>
-      </div>
-      
-    </main>
+    <div className=" flex flex-col  justify-center">
+    <div className="w-full  flex flex-col  p-[32px]  bg-gray-400">
+        
+        <SectionHeader title="จัดการข้อมูลพรรณไม้" backHref="/manage-data/forest"/> 
+       <SectionBelowHeader edit  data={matchedItem} />
+
+        
+        
+
+    </div>
+    </div>
+    
+  
+  
   );
 }
--- src/app/manage-data/land/[fid]/edit/page.tsx
+++ src/app/manage-data/land/[fid]/edit/page.tsx
@@ -1,15 +1,53 @@
+'use client'
+import { mockDataGroups } from "../../.././components/mockDataGroups";
+import { useParams, useRouter } from "next/navigation";
+import { ForestData, LandData } from "../../../../../../types";
+import SectionBelowHeader from "@/app/manage-data/components/SectionBelowHeader";
+import SectionHeader from "@/app/manage-data/components/SectionHeader";
+import InputField from "@/components/InputField";
+import Button from "@/components/Button";
+import ViewDetailDataCard from "@/app/manage-data/components/ViewDetailDataCard";
 
 
 
 export default function Edit() {
+  const { fid } = useParams();
+  const idFromParam = Number(fid);
+
+  let matchedItem: ForestData | LandData | null = null;
+
+  for (const group of mockDataGroups) {
+    if (group.forestData) {
+      matchedItem = group.forestData.find(item => item.id === idFromParam)??null;
+      if (matchedItem) break;
+    }
+    if (group.LandData) {
+      matchedItem = group.LandData.find(item => item.id === idFromParam)??null;
+      if (matchedItem) break;
+    }
+  }
+  if(!matchedItem) return null;
+    const isForest = "tree_type" in matchedItem;
+    const router = useRouter();
+
   return (
-    <main className="flex  items-center justify-center p-4 sm:p-6 md:p-8">
-      <div className="w-full max-w-md h-64 bg-white rounded-xl shadow-2xl flex items-center justify-center p-4">
-          <div className=" font-mono text-3xl font-bold text-center">
-          Edit
-          </div>
-      </div>
-      
-    </main>
+    <div className=" flex flex-col  justify-center">
+    <div className="w-full  flex flex-col  p-[32px]  ">
+        
+    <div className="mb-[24px] ">
+        <SectionHeader title="จัดการข้อมูลแปลงที่ดิน" backHref="/manage-data/land"/> 
+    </div>
+    <div className="mb-[24px] ">
+        <SectionBelowHeader edit land data={matchedItem} />
+    </div>
+
+    <ViewDetailDataCard/>
+        
+
+    </div>
+    </div>
+    
+  
+  
   );
 }
