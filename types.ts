37252acd2104cc9acea3ebf4ffb30f3cb9f5626a
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-20 11:56:52 +0700

    feat(mockdata): add more field and update types

--- types.ts
+++ types.ts
@@ -1,17 +1,23 @@
-export type ForestData={
-    id:number,
-    number:number,
-    name:string,
-    tree_type:string
-    
-}
+export type ForestData = {
+  id: number;
+  number: number;
+  name: string;
+  tree_type: string;
+  location: string;
+  height: string;
+  age: string; 
+};
 
-export type LandData={
-    id:number,
-    number:number,
-    name:string,
-    land_type:string
-}
+export  type LandData = {
+  id: number;
+  number: number;
+  name: string;
+  land_type: string;
+  owner: string;
+  area_size: string;
+  soil_type: string; 
+};
+
 export type DataGroup={
     id:number,
     name:string,
