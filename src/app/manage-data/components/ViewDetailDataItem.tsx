0159e298fd5a9e483a29fdfee8f5fffe9d0931db
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-20 12:58:50 +0700

    feat(ui): add ViewDetailDataItem component

--- src/app/manage-data/components/ViewDetailDataItem.tsx
+++ src/app/manage-data/components/ViewDetailDataItem.tsx
@@ -1,15 +1,34 @@
 import InputField from "@/components/InputField";
+import { useState } from "react";
 
 type ViewDetailDataItemProps ={
     text:string,
+    defaultValue:string,
+    textClass?:string,
+    boxClass?:string
 }
 
-export default function ViewDetailDataItem({text}:ViewDetailDataItemProps){
+export default function ViewDetailDataItem({
+    text,
+    defaultValue,
+    textClass="w-[45%] ",
+    boxClass="w-[55%]"
+
+}:ViewDetailDataItemProps){
+     const [someValue, setSomeValue] = useState(defaultValue);
     return(
-        <div className=" w-[434px] h-[32px] flex flex-row justify-between items-center font-prompt text-base ">
-            <div className="w-[auto] h-[32px] items-center flex ">{text}</div>
-            <InputField className=" w-full h-[34px] rounded-lg "/>
-        </div> 
+        <div className="w-[100%]    h-[32px] flex flex-row  items-center font-prompt text-base ">
+            <div className={` ${textClass} h-[32px] items-center  flex`}>{text}</div>
+            <div className={` ${boxClass} h-[32px] items-center justify-center flex`}>
+            <InputField className="w-full h-full   " 
+            value={someValue}
+            inputClassName="rounded-lg  pl-[10px] "
+            onChange={(e) => setSomeValue(e.target.value)}
+             />
+
+            </div>
+
+        </div>
     );
 
 }
