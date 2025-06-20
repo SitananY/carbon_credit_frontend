75c434bf0a1cda5575374b5032d3753c0c89ee92
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-20 15:21:13 +0700

    chore(ui): add handle button function in ViewDetailDataCard component

--- src/app/manage-data/components/ViewDetailDataCard.tsx
+++ src/app/manage-data/components/ViewDetailDataCard.tsx
@@ -6,10 +6,13 @@ import { ForestData, LandData } from "../../../../types";
 
 type ViewDetailDataCardProps = {
     data: ForestData | LandData;
-    isForest:boolean
+    isForest:boolean;
+    onConfirm?: ()=>void;
+    onCancle? : ()=>void
+
 }
 
-export default function ViewDetailDataCard({ data , isForest}: ViewDetailDataCardProps) {
+export default function ViewDetailDataCard({ data , isForest , onConfirm , onCancle}: ViewDetailDataCardProps) {
   
   const entries = data ? Object.entries(data) : []; //แปลง object ให้กลายเป็น array คู่ [key,value]
   const midpoint = Math.ceil(entries.length / 2);
@@ -17,7 +20,7 @@ export default function ViewDetailDataCard({ data , isForest}: ViewDetailDataCar
   const rightItems = entries.slice(midpoint);
   
   return (
-    <div>
+    <div className="relative">
       {/* กล่องบน */}
       <div className="mb-6 rounded-lg bg-[#B6B6A8] h-[56px] max-lg:h-[112px]  px-8 flex flex-row max-lg:flex-col items-center justify-center gap-[66px] max-lg:gap-5">
         <div className="w-[43%] max-lg:w-[90%] h-[32px] flex flex-row items-center font-prompt text-base">
@@ -30,7 +33,7 @@ export default function ViewDetailDataCard({ data , isForest}: ViewDetailDataCar
       </div>
 
       {/* กล่องกลาง */}
-      <div className="mb-6 bg-[#B6B6A8] w-full h-auto max-lg:h-auto rounded-lg px-[32px] py-[16px] flex flex-row max-lg:flex-col max-lg:items-center justify-center max-lg:gap-[32px] gap-[66px]">
+      <div className="mb-6 bg-[#B6B6A8] w-full h-auto max-lg:h-auto rounded-lg px-[32px] py-[16px] flex flex-row max-lg:flex-col max-lg:items-center justify-center max-lg:gap-4 gap-[66px]">
         <div className="w-[43%] max-lg:w-[90%] flex flex-col gap-4">
           {leftItems.map(([key, value], index) => (
             <ViewDetailDataItem key={index} text={key} defaultValue={String(value ?? "")} />
@@ -56,8 +59,8 @@ export default function ViewDetailDataCard({ data , isForest}: ViewDetailDataCar
       <div className="mb-6  w-full h-[40px] font-prompt text-base flex flex-row justify-between items-center ">
         <div>ผู้บันทึกข้อมูล user1 วันที่ 12 มิถุนายน 2568 เวลา 9:18 น.</div>
         <div className="flex flex-row justify-between items-center gap-8 max-md:gap-5">
-          <Button text="ยืนยัน" variant="confirm" className="w-[85px] h-[40px] rounded-lg" />
-          <Button text="ยกเลิก" variant="cancle" className="w-[85px] h-[40px] rounded-lg" />
+          <Button text="ยืนยัน" variant="confirm" className="w-[85px] h-[40px] rounded-lg" onClick={onConfirm} />
+          <Button text="ยกเลิก" variant="cancle" className="w-[85px] h-[40px] rounded-lg" onClick={onCancle}/>
         </div>
       </div>
     </div>
