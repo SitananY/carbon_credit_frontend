e26a618ac573de8d43d5d625bc05529bcc1d62cb
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-23 13:08:03 +0700

    feat(ui):  layout dashboard  page based on wireframe

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,30 @@
-
-import { PieChart } from '@mui/x-charts';
-import PieChartWithCustomLegendAndTooltip from './components/PieChartDemo';
-import PieChartDemo from './components/PieChartDemo';
-import Button from '@/components/Button';
+import SectionBelowHeader from "../../components/SectionBelowHeader";
+import SectionHeader from "../../components/SectionHeader";
+import ViewDataCard from "../manage-data/components/ViewDataCard";
 
 export default function Dashboard() {
   return (
-    <main className="flex flex-col items-center justify-center p-4 sm:p-6 md:p-8 ">
-      <div className="w-full max-w-md h-64 bg-white rounded-xl shadow-2xl flex items-center justify-center p-4">
-          <div className=" font-mono text-3xl font-bold text-center">
-          Dashboard
-          </div>
-      </div>
-      <div className='my-10 w-full max-w-md h-64 bg-white rounded-xl shadow-2xl flex items-center justify-center p-4'>
-        <PieChartDemo/>
-      </div>
- 
-    </main>
+    <div className=" flex flex-col  justify-center">
+        <div className="w-full  flex flex-col  py-[32px] xl:px-[148px] px-[32px]">
+            
+            <SectionHeader title="แสดงผล Dashboard" backHref="/" />
+            
+            <div className="py-[24px]"><SectionBelowHeader dashboard /> </div>
+
+            <div className="w-full h-full   flex flex-col gap-[30px] font-prompt font-medium text-[#27272A] text-xl">
+                  <div className="w-full h-[200px]  flex flex-row gap-[40px]">
+                      <div className="w-full h-full bg-[#DDDDDC] pl-[16px]"><div className="h-[54px] w-[auto] flex items-center " >ศักยภาพการกักเก็บคาร์บอน</div></div>
+                      <div className="w-full h-full bg-[#DDDDDC] pl-[16px]"><div className="h-[54px] w-[auto] flex items-center " >พื้นที่ป่า</div></div>
+                  </div>
+                  <div className="w-full h-[286px] bg-[#DDDDDC] flex pl-[16px] ">
+                    <div className="h-[54px] w-[auto] flex items-center " >แนวโน้มปริมาณการกักเก็บคาร์บอนตามปี</div>
+                  </div>
+                  <div className="w-full h-[286px] bg-[#DDDDDC] flex pl-[16px]">
+                    <div className="h-[54px] w-[auto] flex items-center " >ปริมาณการกักเก็บคาร์บอนตามระบบนิเวศ</div>  
+                  </div>
+            </div>
+        </div>
+        
+        </div>
   );
 }
