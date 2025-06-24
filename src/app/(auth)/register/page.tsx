d7813955d1f1a4e593d739fa3d0791cfe3f67e33
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-24 17:30:24 +0700

    feat(ui): layout register  page based on wireframe

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,15 +1,60 @@
+'use client'
+import Button from "@/components/Button";
+import InputField from "@/components/InputField";
+import TestCard from "@/components/Testcard";
+import Icons from "@/components/svgs/SvgExports"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 
 export default function Register() {
+  const [isVisible,setIsVisible] = useState(false);
+  const router = useRouter();
+   const handleClick = () => {
+          const path = `/login`;
+          router.push(path);
+    }
+
   return (
-    <main className="flex  items-center justify-center p-4 sm:p-6 md:p-8">
-      <div className="w-full max-w-md h-64 bg-white rounded-xl shadow-2xl flex items-center justify-center p-4">
-          <div className=" font-mono text-3xl font-bold text-center">
-          Register
-          </div>
-      </div>
-      
-    </main>
+    <div className="w-screen h-screen flex  items-center justify-center ">
+        <div className="w-[40%] max-md:w-[70%] h-[70%] bg-[#DDDDDC] flex items-center justify-center rounded-[56px] 
+        pb-[62px] pt-[42px] pl-[80px] pr-[80px] 
+        max-md:pb-[30px] max-md:pt-[38px]  max-md:pl-[40px] max-md:pr-[40px]
+        font-prompt font-medium">
+              <div className="w-full  h-full flex flex-col justify-between items-center gap-[20px]  ">
+                    <div className="flex justify-center items-center text-[36px]  w-full h-full ">
+                      สร้างบัญชี
+                    </div>
+                    <div className="flex flex-row gap-[30px] ">
+                         <div className="flex flex-col w-full gap-[16px] text-[20px]  ">
+                          ชื่อ 
+                          <InputField className=" w-full h-[52px]"/>  
+                        </div>
+                        <div className="flex flex-col w-full gap-[16px] text-[20px] ">
+                          นามสกุล 
+                          <InputField className="w-full h-[52px]" />  
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-[20px]  w-full h-full items-center "> 
+                        <div className="flex flex-col w-full gap-[16px] text-[20px]  ">
+                          อีเมล 
+                          <InputField className=" w-full h-[52px]"/>  
+                        </div>
+                        <div className="flex flex-col w-full gap-[16px] text-[20px] ">
+                          รหัสผ่าน 
+                          <InputField className="w-full h-[52px]" rightItem={isVisible ?<Icons.Visibility onClick={()=>setIsVisible(!isVisible)}/> :<Icons.Visibility_off onClick={()=>setIsVisible(!isVisible)}/>} canPoint=""/>  
+                        </div>
+                    </div>
+                    <div className="flex flex-col w-full h-full ">
+                          <Button text="สร้างบัญชี" className="w-full h-[52px] rounded-[32px]" textClassName="text-[20px] " onClick={handleClick}/>
+                          <div className="flex flex-row justify-end items-center text-[18px] ">
+                                <Link href={"/login"} className="hover:underline" >มีบัญชีแล้ว?</Link>
+                          </div>
+                    </div>
+              </div>
+        </div>
+    </div>
   );
 }
