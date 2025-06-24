c3fbcfaf66298b55f5b21212cabe1b95da72b331
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-24 15:28:02 +0700

    feat(sidebar):add handler for specific layout of (auth) route group and map page

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,4 +1,4 @@
-// components/ClientLayout.tsx
+
 'use client'
 
 import { usePathname } from 'next/navigation'
@@ -7,13 +7,30 @@ import SideBarCard from '@/components/SideBarCard'
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
   const isMapPage = pathname === '/map'
+  const isAuthPage = pathname.startsWith('/login') || pathname.startsWith('/register') || pathname.startsWith('/change-password')
+  
+  if(isAuthPage){
+    return <>{children}</>
+  }
+
 
   return (
     
 
-        <div className="flex h-screen">
-          <SideBarCard forcedCollapsed={isMapPage?true:false}/>
-          <main className="flex-1 overflow-auto">{children}</main>
+       <div className={`h-screen w-screen ${isMapPage ? "relative overflow-hidden" : "flex"}`}>
+          <div className={isMapPage ? "absolute z-20 h-full" : "relative"}>
+            <SideBarCard forcedCollapsed={isMapPage} />
+          </div>
+
+          <main
+            className={`
+              h-full
+              ${isMapPage ? "absolute top-0 left-0 ml-[117px] w-full z-10" : "flex-1"}
+              overflow-auto
+            `}
+          >
+            {children}
+          </main>
         </div>
       
   
