2b8702d9ca9c6a2a2cee1426a6399bc2f1b1aedf
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-25 17:06:44 +0700

    chore(ui) : add whitespace-nowrap for text in popup component

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -23,11 +23,12 @@ export default function Popup({isShow,children,onCancle,onConfirm,add,remove,edi
             <div className="w-[350px] h-[200px]  rounded-[40px] bg-[#FAFCFE] shadow-xl px-[40px] items-center justify-center flex ">
                     <div className="w-[246px] h-[118px]  flex flex-col gap-[16px] items-center justify-center">
                         <div className="w-full h-[54px]   font-medium font-prompt text-xl flex flex-col items-center justify-center  " >
-                               {add && "คุณต้องการเพิ่มข้อมูลนี้หรือไม่"}
-                                {remove && "คุณต้องการลบข้อมูลนี้หรือไม่"}
-                                {edit && "คุณต้องการแก้ไขข้อมูลนี้หรือไม่"}
-                                {warning && "ข้อมูลไม่ถูกต้อง"}
-                                <div className="w-auto whitespace-nowrap">{warning && "โปรดตรวจสอบก่อนทำการยืนยัน"}</div>
+                               <span className="whitespace-nowrap w-auto">
+                                    {add && "คุณต้องการเพิ่มข้อมูลนี้หรือไม่"}
+                                    {remove && "คุณต้องการลบข้อมูลนี้หรือไม่"}
+                                    {edit && "คุณต้องการแก้ไขข้อมูลนี้หรือไม่"}
+                                    {warning && "ข้อมูลไม่ถูกต้อง โปรดตรวจสอบก่อนทำการยืนยัน"}
+                                </span>
 
                         </div>
                         <div className={`w-[246px] h-[48px]  flex flex-row gap-[30px] px-[32px] items-center justify-center 
