f8a90194542c8cd2df08b5273b767f7d95d1c063
SitananYaowayang <6733270421@student.chula.ac.th>
2025-06-30 14:57:55 +0700

    feat(ui) : add DataTableItem2 component from design system

---
+++ src/components/DataTableItem2.tsx
@@ -0,0 +1,56 @@
+import CheckBox from "@/components/CheckBox";
+import Icons from "@/components/svgs/SvgExports"
+type DataTableItem2={
+    isText?:boolean,
+    isCheckbox?:boolean,
+    isAction?:boolean,
+    action?: React.ReactNode ,
+    text?:string,
+    parentClassName?:string,
+    childClassName?:string,
+    selected?:boolean,
+    disabled?:boolean,
+    header?:boolean,
+    onClick?:()=> void,
+    onHeaderClick?: ()=>void,
+}
+
+export default function DataTableItem2 ({
+    isText,
+    isCheckbox,
+    isAction,
+    action,
+    text,
+    parentClassName="flex flex-row items-center px-[16px] py-[10px]",
+    childClassName="",
+    selected,
+    disabled,
+    header,
+    onClick,
+    onHeaderClick,
+
+}:DataTableItem2){
+    const style = 
+    selected
+    ? "font-prompt text-base text-cancel-700 bg-nuetral-500 border-neutral-700  "
+    :disabled 
+    ? "font-prompt text-base text-cancel-700  bg-cancel-300 border-cancel-300  " 
+    :header 
+    ? "font-prompt font-medium text-base text-text-800 bg-gray-v1 border-cancel-700" 
+    : "font-prompt text-base text-text-800 bg-neutral-300 border-neutral-700 hover:bg-gray-v1 " ;
+
+    
+    return(
+        <div onClick={onClick} className={` h-full w-full border-b-1 ${parentClassName} ${style}`}>
+            <>
+                {isText && text}
+                {isCheckbox && <CheckBox isChecked={selected} disabled={disabled} onClick={onClick}/>}
+                {isAction && action}
+                {header && <Icons.Down className="w-[18px] h-[18px]"  onClick={onHeaderClick}/> }
+                
+            </>        
+        </div>
+        
+        
+    );
+}
