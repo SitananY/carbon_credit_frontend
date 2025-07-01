ecd490fae674b72f63fff3a1e7a9b716d77cf24f
SitananYaowayang <6733270421@student.chula.ac.th>
2025-07-01 13:59:18 +0700

    feat(mockdata) : add type for mockdata

--- types.ts
+++ types.ts
@@ -26,3 +26,52 @@ export type DataGroup={
 
 
 }
+
+
+export type Tree = {
+  scientificName: string;
+  commonName: string;
+  family: string;
+  otherNames: string[];
+  type: string;
+  trunkCharacteristic: string;
+  averageHeightM: number;
+  diameterCm: number;
+  approxAgeY: number;
+  quantity: number;
+  latitude: number;
+  longitude: number;
+};
+
+export type SurveyImages = {
+  landImage: string;
+  treeImages: string[];
+};
+
+export type GroupType = "ป่าดงดิบ" | "ป่าผสมผลัดใบ" | "ป่าเต็งรัง" | "ป่าชายเลน" | "ป่าสนเขา";
+
+export type LandPlot = {
+  id:string;
+  landNumber: string;
+  budgetYear: number;
+  deedNumber: string;
+  surveyPage: string;
+  sheet: string;
+  area: string;
+  latitude: number;
+  longitude: number;
+  province: string;
+  district: string;
+  subdistrict: string;
+  forestType: GroupType;
+  trees: Tree[];
+  surveyImages: SurveyImages;
+};
+
+export type LandPlotGroup = {
+  groupId:string;
+  groupType:GroupType;
+  plots: LandPlot[];
+  itemCount: number;        
+  latestUpdate: string;  
+};
