c0ecd45bf520ef8078a71eb0fc45129fd5ff38df
SitananYaowayang <6733270421@student.chula.ac.th>
2025-07-02 18:44:39 +0700

    feat(ui) : add  demo query mockdata in table  in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Badge from "@/components/Badge";
 import CheckBox from "@/components/CheckBox";
 import DataTableGeneral from "@/components/DataTableGeneral";
 import DataTableItem2 from "@/components/DataTableItem2";
+import SortFilterDropdown from "@/components/SortFilterDropdown";
 import TestCard from "@/components/Testcard";
 import ToolTip from "@/components/ToolTip";
 import { mockLandPlotGroups } from "@/mockDataGroups";
@@ -13,9 +14,57 @@ import { useState } from "react";
 export default function Home() {
   const [isClick,setIsClick] = useState(false);
   const [data,setData]= useState(mockLandPlotGroups);
+   const [filters, setFilters] = useState<string[]>([]);
+  const [sortOrder, setSortOrder] = useState<string | null>(null);
+
+  
+  const applyFilters = (filters: string[], dataToFilter = mockLandPlotGroups) => {
+    if (filters.length === 0) return dataToFilter;
+    return dataToFilter.filter(item => filters.includes(item.groupType));
+  };
+
+  
+  const applySort = (order: string | null, dataToSort: any[]) => {
+    if (!order) return dataToSort;
+    const sorted = [...dataToSort].sort((a, b) =>
+      order === "น้อยไปมาก"
+        ? a.itemCount - b.itemCount
+        : b.itemCount - a.itemCount
+    );
+    return sorted;
+  };
+
+  
+  const updateData = (newFilters = filters, newSortOrder = sortOrder) => {
+    let filtered = applyFilters(newFilters);
+    let sorted = applySort(newSortOrder, filtered);
+    setData(sorted);
+  };
+
   return (
     <main className="flex flex-col items-center justify-center p-4 sm:p-6 md:p-8 gap-5 ">
-          
+
+      <SortFilterDropdown
+        type="sort"
+        options={["น้อยไปมาก", "มากไปน้อย"]}
+        onSelect={(selected) => {
+          console.log("Sort by", selected);
+          setSortOrder(selected as string);
+          updateData(filters, selected as string);
+        }}
+      />
+
+      <SortFilterDropdown
+        type="filter"
+        options={["ป่าดงดิบ", "ป่าผสมผลัดใบ", "ป่าสนเขา"]}
+        onSelect={(selectedFilters) => {
+          console.log("Filter by", selectedFilters);
+          setFilters(selectedFilters as string[]);
+          updateData(selectedFilters as string[], sortOrder);
+        }}
+      />
+
+
       <div className="mb-[50px]">
       <DataTableGeneral data={data} group />
       </div>
