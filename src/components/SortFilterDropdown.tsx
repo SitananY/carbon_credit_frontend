fd38183535c3fff08224c40db87a81fa54a98553
SitananYaowayang <6733270421@student.chula.ac.th>
2025-07-07 09:37:15 +0700

    feat(ui) : debug icon.down in SortFilterDropdown component

--- src/components/SortFilterDropdown.tsx
+++ src/components/SortFilterDropdown.tsx
@@ -6,7 +6,7 @@ import CheckBox from "@/components/CheckBox";
 import ListItem from "@/components/ListItem";
 type SortFilterDropdownProps = {
   type: "sort" | "filter";
-  options: string[];
+  options?: string[];
   onSelect: (selected: string | string[]) => void;
 };
 
@@ -31,7 +31,12 @@ export default function SortFilterDropdown({ type, options, onSelect }: SortFilt
       onSelect(newSelected);
     }
   };
+   const sortOptions = [
+        { label: "เรียงจากน้อยไปมาก", icon: <Icons.Arrow_upward className = "w-[18px] h-[18px]"/> },
+        { label: "เรียงจากมากไปน้อย", icon: <Icons.Arrow_downward className = "w-[18px] h-[18px]"/> },
+      ];
 
+    if(!options) return null ;
   return (
     <div className="relative inline-block">
 
@@ -55,33 +60,36 @@ export default function SortFilterDropdown({ type, options, onSelect }: SortFilt
         </Button>
       )}
 
+     
       {/* แยก dropdown style ตาม type */}
+     
     {isOpen && (
         <div className={`shadow-lg  z-10 absolute top-full right-0 ${type==="sort"?"" :"w-[341px] border border-neutral-700 rounded-xl p-[16px] gap-[10px] bg-white flex flex-col"  }`}>
-          {options.map((option, idx) => (
-            type === "sort" ? (
-              <ListItem 
-                key={idx}
-                className="h-[32px] w-[203px]"
-                textPxGap="pl-[16px]" 
-                item={option}
-                onClick={() => handleSelect(option)}
-                selected={selected === option}
-              />
-            ) : (
+          {type === "sort" ? 
+              (sortOptions.map((opt, idx) => (
+                  <ListItem
+                    key={idx}
+                    className="h-[46px] w-[203px]"
+                    textPxGap="pl-[16px]"
+                    onClick={() => handleSelect(opt.label)}
+                    selected={selected === opt.label}
+                  >
+                   <div className="flex  items-center  gap-5">{opt.icon}  {opt.label}</div> 
+                  </ListItem>
+                ))
+              ) : (
                 
-                    <div
-                        key={idx}
-                        className=" w-full h-full flex items-center gap-[10px] font-prompt    bg-white "
-                        onClick={() => handleSelect(option)}
-                    >
-                        <CheckBox isChecked={(selected as string[]).includes(option)} />
-                        <span>{option}</span>
-                    </div>
-                
-              
-            )
-          ))}
+                options.map((option, idx) => (
+                  <div
+                    key={idx}
+                    className="w-full h-full flex items-center gap-[10px] font-prompt bg-white"
+                    onClick={() => handleSelect(option)}
+                  >
+                    <CheckBox isChecked={(selected as string[]).includes(option)} />
+                    <span>{option}</span>
+                  </div>
+                ))
+              )}
         </div>
       )}
     </div>
