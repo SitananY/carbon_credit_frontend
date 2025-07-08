f0243c95076983ecdd56c3f8e1c127de3af75436
SitananYaowayang <6733270421@student.chula.ac.th>
2025-07-08 17:25:31 +0700

    feat(ui) :add checkbox handler system in DataTableRow component

--- src/components/DataTableRow.tsx
+++ src/components/DataTableRow.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import DataTableItem2 from "./DataTableItem2";
+import Icons from "@/components/svgs/SvgExports"
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type DataTableRowProps = {
   data: any; // data ของแถวนี้
   group?: boolean;
   landPlot?: boolean;
   className?:string;
-    disabled?:boolean;
+  disabled?:boolean;
+  selectedIds: string[];
+  setSelectedIds?: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
 export default function DataTableRow({
@@ -14,78 +19,119 @@ export default function DataTableRow({
   group = false,
   landPlot = false,
   className =" w-full h-full",
-  disabled
+  disabled,
+  selectedIds,
+  setSelectedIds
 }: DataTableRowProps) {
   const [isClick, setIsClick] = useState(false);
 
   if (group) {
     return (
+      <Link href={`/manage-data/${encodeURIComponent(data.forestType)}`}>
+      
       <div className={` ${className} flex flex-row  ${isClick ?"" :disabled?"":"bg-neutral-300  hover:bg-gray-v1"}  `}
         
       >
         {/* <div className="bg-cancel-300 h-full w-[20%]"></div> */}
         <DataTableItem2
           isCheckbox
-          selected={isClick}
-          onClick={() => setIsClick(!isClick)}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
           parentClassName="w-[10%] "
         />
         <DataTableItem2
           isText
-          selected={isClick}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
+  
           text={data.forestType}
           parentClassName="w-[45%] "
         />
         <DataTableItem2
           isText
-          selected={isClick}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
+  
           text={`${data.lands.length} รายการ`}
           parentClassName="w-[20%] "
         />
         <DataTableItem2
           isText
-          selected={isClick}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
           text={data.latestUpdate}
           parentClassName="w-[25%] "
         />
       </div>
+      </Link>
     );
   }
 
   if (landPlot) {
     return (
+      <Link href={`/manage-data/${encodeURIComponent(data.forestType)}/`}>
+       
       <div className="w-full flex flex-row">
         <DataTableItem2
           isCheckbox
-          selected={isClick}
-          onClick={() => setIsClick(!isClick)}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
           parentClassName="w-[10%] "
         />
         <DataTableItem2
           isText
-          selected={isClick}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
           text={data.landNumber}
           parentClassName="w-[20%] "
         />
         <DataTableItem2
           isText
-          selected={isClick}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
+
           text={data.province}
           parentClassName="w-[30%] "
         />
         <DataTableItem2
           isText
-          selected={isClick}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
+
           text={data.area}
           parentClassName="w-[20%] "
         />
         <DataTableItem2
           isAction
-          selected={isClick}
-          action={<button>ดู</button>}
+          rowId={data.id}
+          selectedIds={selectedIds}
+          setSelectedIds={setSelectedIds}
+          selected={selectedIds.includes(data.id)}
+
+          action={
+          <Icons.Edit className="w-[18px] h-[18px]"/>
+          }
           parentClassName="w-[20%] "
         />
       </div>
+       
+      </Link>
     );
   }
 
