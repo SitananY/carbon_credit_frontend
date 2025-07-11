7f13b8829e1c9b68496b0e87e70f71bfdd8b72ac
SitananYaowayang <6733270421@student.chula.ac.th>
2025-07-11 17:00:04 +0700

    feat(ui) :layout style in Process Data page component

--- src/app/process-data/page.tsx
+++ src/app/process-data/page.tsx
@@ -1,41 +1,60 @@
+"use client"
 import Button from "@/components/Button";
 import InputField from "@/components/InputField";
 import ListItem from "@/components/ListItem";
 import SectionBelowHeader from "@/components/SectionBelowHeader";
 import SectionHeader from "@/components/SectionHeader";
 import Icons from "@/components/svgs/SvgExports";
-import ProcessDataCard from "@/app/process-data/components/processDataCard";
+import ProcessDataCard from "@/app/process-data/components/ProcessDataCard";
+import { useState } from "react";
+import DataTableGeneral from "@/components/DataTableGeneral";
 
 
 export default function ProcessData() {
-  
-
+  const [textTitle,setTextTitle] = useState("เลือกกลุ่มสูตร")
+  const [isDisabled,setIsDisabled] = useState(false)
+  const [formulaTitle,setFormulaTitle] = useState("ระบุชื่อสูตร")
+  const [formulaText,setFormulaText] = useState("ระบุสูตร")
+  const processHandle = (textTitle?:string , isDisabled?:boolean 
+    ,formulaTitle?:string , formulaText?:string)=>{
+      if(textTitle)setTextTitle(textTitle);
+      if(isDisabled)setIsDisabled(isDisabled);
+      if(formulaText){
+          setFormulaText(formulaText);
+      }
+      if(formulaTitle){
+          setFormulaTitle(formulaTitle);
+      }
+      
+      
+  }
+  // const formulaHandle = (formulaTitle:string , formulaText:string)=>{
+  //     setFormulaText(formulaText);
+  //     setFormulaTitle(formulaTitle);
+  // }
+  const [isDescrip,setIsDescrip] = useState(false)
+ 
+console.log(isDisabled);
   return (
-     <div className=" flex flex-col  justify-center">
-            <div className="w-full h-full  flex flex-col  py-[32px] xl:px-[148px] px-[32px]">
-                
+     <div className="flex flex-col justify-center">
+      <div className="w-full flex flex-col items-center py-[32px] xl:px-[148px] px-[32px] gap-7">
+                                
                 <SectionHeader title="ประมวลผลข้อมูล" backHref />
+                <SectionBelowHeader isEditProcess={true} process processTextTitle={textTitle} processHandle={processHandle}/>                
+                <ProcessDataCard isDisabled={isDisabled} processHandle={processHandle} />
                 
-                
-    
-                <div className="w-full h-[30%] px-[32px] my-[24px] py-[16px] gap-[16px] bg-[#B6B6A8] rounded-2xl flex flex-col  font-prompt text-[#27272A]">
-                      <div className="flex flex-row justify-between items-center ">
-                          <div className="text-xl font-medium">สูตรการคำนวณ</div>
-                          <Button text="เพิ่ม" className="w-[74px] h-[40px]  rounded-lg" ><Icons.Add/></Button>
-                      </div>
-                      <div className="h-[69px] w-full text-sm flex flex-col">
-                          ชื่อข้อมูล
-                          <ListItem className="w-full  h-[48px]" item="ชื่อสูตร"   textPxGap="pl-[12px]"/>
-                      </div>
-                      <div className="h-[69px] w-full text-sm flex flex-col">
-                          สูตรการกักเก็บคาร์บอน
-                          <InputField placeholder="สูตรกักเก็บคาร์บอน" className="w-full h-[48px]" inputClassName="pl-[10px] rounded-xl"/>
-                      </div>
-                </div>
-
-                <div className="w-full h-[60%]  ">
-                        <ProcessDataCard />
-                </div>
+                <div  className="h-[auto] w-full flex flex-col  gap-[20px]" >
+                  <Button text="คำอธิบายตัวแปร" variant="tonal" className="w-full h-[40px] rounded-lg pl-[15px] " alignment="" onClick={()=>{setIsDescrip(!isDescrip)}}>
+                  {isDescrip ? 
+                  <>
+                      <Icons.Down className="w-[18px] h-[18px]"/> 
+                  </>
+                  
+                  :   <Icons.Forward className="w-[18px] h-[18px]"/>}
+                  </Button>
+                  {isDescrip && <DataTableGeneral data={[]} selectedIds={[]} setSelectedIds={()=>{}} isPage={false} progress />}
+                </div>          
+            
             </div>
             
             </div>
